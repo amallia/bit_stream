23b9e800e8800ec5bf135520a5a0b61355d02580
Antonio Mallia <me@antoniomallia.it>
2018-11-08 21:05:23 -0500

    Added bytes fuction

--- test/bit_stream_test.cpp
+++ test/bit_stream_test.cpp
@@ -28,58 +28,73 @@ TEST(bit_stream, bits) {
     EXPECT_EQ(bits(0b10), 2);
     EXPECT_EQ(bits(0b11), 2);
     EXPECT_EQ(bits(0b100), 3);
-    EXPECT_EQ(bits((1ULL<<63)), 64);
-    EXPECT_EQ(bits((1ULL<<63) - 1), 63);
+    EXPECT_EQ(bits((1ULL << 63)), 64);
+    EXPECT_EQ(bits((1ULL << 63) - 1), 63);
+}
+
+TEST(bit_stream, bytes) {
+
+    EXPECT_EQ(bytes(0), 1);
+    EXPECT_EQ(bytes(255), 1);
+    EXPECT_EQ(bytes(256), 2);
+    EXPECT_EQ(bytes(65535), 2);
+    EXPECT_EQ(bytes(65536), 3);
 }
 
 TEST(bit_stream, test) {
     {
-        std::vector<uint8_t> buffer(4);
+        auto value = 100;
+        std::vector<uint8_t> buffer(bytes(value));
         bit_ostream          bo(buffer.data());
-        bit_istream bi(buffer.data());
-        bo.write(100, 8);
-        auto value = bi.read(8);
-        EXPECT_EQ(value, 100);
+        bit_istream          bi(buffer.data());
+        bo.write(value, 8);
+        auto decoded_value = bi.read(8);
+        EXPECT_EQ(decoded_value, value);
     }
     {
-        std::vector<uint8_t> buffer(4);
+        auto value = 100;
+        std::vector<uint8_t> buffer(bytes(value));
         bit_ostream          bo(buffer.data());
-        bit_istream bi(buffer.data());
-        bo.write_unary(100);
-        auto value = bi.read_unary();
-        EXPECT_EQ(value, 100);
+        bit_istream          bi(buffer.data());
+        bo.write_unary(value);
+        auto decoded_value = bi.read_unary();
+        EXPECT_EQ(decoded_value, value);
     }
     {
-        std::vector<uint8_t> buffer(4);
+        auto value = 100;
+        std::vector<uint8_t> buffer(bytes(value));
         bit_ostream          bo(buffer.data());
-        bit_istream bi(buffer.data());
-        bo.write_elias_gamma(100);
-        auto value = bi.read_elias_gamma();
-        EXPECT_EQ(value, 100);
+        bit_istream          bi(buffer.data());
+        bo.write_elias_gamma(value);
+        auto decoded_value = bi.read_elias_gamma();
+        EXPECT_EQ(decoded_value, value);
     }
     {
-        std::vector<uint8_t> buffer(4);
+        auto value = 100;
+        std::vector<uint8_t> buffer(bytes(value));
         bit_ostream          bo(buffer.data());
-        bit_istream bi(buffer.data());
-        bo.write_elias_delta(100);
-        auto value = bi.read_elias_delta();
-        EXPECT_EQ(value, 100);
+        bit_istream          bi(buffer.data());
+        bo.write_elias_delta(value);
+        auto decoded_value = bi.read_elias_delta();
+        EXPECT_EQ(decoded_value, value);
     }
     {
-        std::vector<uint8_t> buffer(4);
+        auto value = 100;
+        std::vector<uint8_t> buffer(bytes(value));
         bit_ostream          bo(buffer.data());
-        bit_istream bi(buffer.data());
-        bo.write_vbyte(100);
-        auto value = bi.read_vbyte();
-        EXPECT_EQ(value, 100);
+        bit_istream          bi(buffer.data());
+        bo.write_vbyte(value);
+        auto decoded_value = bi.read_vbyte();
+        EXPECT_EQ(decoded_value, value);
     }
     {
-        std::vector<uint8_t> buffer(4);
+        auto value = (1ULL << 28) - 1;
+        std::vector<uint8_t> buffer(bytes(value));
         bit_ostream          bo(buffer.data());
-        bit_istream bi(buffer.data());
-        bo.write_vbyte((1ULL<<28)-1);
-        auto value = bi.read_vbyte();
-        EXPECT_EQ(value, (1ULL<<28)-1);
+        bit_istream          bi(buffer.data());
+        bo.write_vbyte(value);
+        auto decoded_value = bi.read_vbyte();
+        EXPECT_EQ(decoded_value, value);
     }
 }
 
